86c2b958637d06f63d78a0a667d99b37202d6156
Magali Fabri <magali_90@live.nl>
2022-01-24 20:01:25 +0100

    Add info icons with tooltips

--- script.js
+++ script.js
@@ -37,31 +37,19 @@ function processData(weatherData) {
     console.log(weatherData);
     for (listItem of weatherData.list) {
         const item = {};
-        // console.log(listItem.weather[0].main);
         item["summary"] = listItem.weather[0].main;
-        // console.log(listItem.weather[0].description);
         item["description"] = listItem.weather[0].description;
-        // console.log(listItem.dt_txt.slice(11, 13));
-        // item["time"] = listItem.dt_txt.slice(11, 13);
         item["date"] = new Date(listItem.dt * 1000);
-        // console.log(listItem.main.temp)
         item["temperature"] = listItem.main.temp;
-        // console.log(listItem.main.humidity)
         item["humidity"] = listItem.main.humidity;
-        // console.log(listItem.clouds.all)
         item["clouds"] = listItem.clouds.all;
-        // console.log(listItem.pop)
         item["precip"] = listItem.pop;
-        // console.log(listItem.weather[0].icon);
         item["iconName"] = listItem.weather[0].icon;
-        // if (listItem.pop > 0) {
-        //     console.log(listItem);
-        // }
         item["wind-speed"] = listItem.wind.speed; // meter/sec
         parsedData.push(item);
     }
-    console.log(parsedData);
-    console.log(parsedData[0]["date"].getHours());
+    // console.log(parsedData);
+    // console.log(parsedData[0]["date"].getHours());
 
     createDOMElements();
 }
@@ -90,16 +78,43 @@ function createDOMElements() {
             newWrapperInfoDiv.classList.add("wrapper-info");
             // insert info wrapper into day wrapper
             newWrapperDayDiv.append(newWrapperInfoDiv);
+
+            // insert info icons with tooltip at start of rows
+            // time
+            const newTimeInfoP = createP("time", "i");
+            addTooltip(newTimeInfoP, "time of day");
+            // temperature
+            const newTemperatureInfoP = createP("temperature", "i");
+            addTooltip(newTemperatureInfoP, "temperature in Celsius");
+            // chance of precipitation
+            const newPrecipitationInfoP = createP("precipitation", "i");
+            addTooltip(newPrecipitationInfoP, "probability of precipitation in %")
+            // humidity
+            const newHumidityInfoP = createP("humidity", "i");
+            addTooltip(newHumidityInfoP, "humidity in %");
+            // wind speed km/h
+            const newWindSpeedInfoP = createP("wind-speed", "i");
+            addTooltip(newWindSpeedInfoP, "wind speed in km/h");
+
+            // newWrapperInfoDiv.append(newTimeInfoP);
+
+            // create time section wrapper and insert base data items
+            const newInfoIconsDiv = document.createElement("div");
+            newInfoIconsDiv.classList.add("info-icons");
+            newInfoIconsDiv.append(newTimeInfoP, newTemperatureInfoP, newPrecipitationInfoP, newHumidityInfoP, newWindSpeedInfoP);
+            
+            // add time section to info wrapper
+            newWrapperInfoDiv.append(newInfoIconsDiv);
         }
 
         const currentDayWrapper = wrapperWeekDiv.children[wrapperWeekDiv.children.length - 1];
         const currentInfoWrapper = currentDayWrapper.children[1];
-        
+
         // create base data items for time section
         const newTimeP = createP("time", item.date.getHours() + "u");
         const newSummaryP = createP("summary", item.summary);
         const newTempP = createP("temperature", convertKelvinToCelsius(item.temperature) + "°");
-        const newPrecipitationP = createP("precipitation", (item.precip * 100) + "%");
+        const newPrecipitationP = createP("precipitation", Math.round(item.precip * 100) + "%");
         const newHumidityP = createP("humidity", item.humidity + "%");
         const newWindSpeedP = createP("wind-speed", Math.round(item["wind-speed"] * 3.6));
         const newIconImg = document.createElement("img");
@@ -122,6 +137,14 @@ function createP(className, content) {
     newP.textContent = content;
 
     return (newP);
+}
+
+function addTooltip(parentElem, tooltipContent) {
+    const newTooltip = document.createElement("div");
+    newTooltip.classList.add("tooltip");
+    newTooltip.textContent = tooltipContent;
+    
+    parentElem.append(newTooltip);
 }
 
 function convertKelvinToCelsius(F) {
