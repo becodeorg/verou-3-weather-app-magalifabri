f2763a73a033035740872644f832da4d39565ea8
Magali Fabri <magali_90@live.nl>
2022-01-24 11:17:45 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// fetch('http://example.com/movies.json')
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  
