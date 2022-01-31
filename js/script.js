4b37a7ceced6718589884ab19d6f47e7ac55d0c5
Magali Fabri <magali_90@live.nl>
2022-01-31 15:28:11 +0100

    Refactor
    - Change function declarations to arrow functions
    - await fetch() instead of using .then and chaining function calls
    - Reduce globals

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ import dummyOneCallAPIData from "./dummyOneCallAPIData.js";
 import { createCurrentWeatherDiv } from "./current-weather.js";
 import { createComingDaysSection } from "./coming-days.js";
 
+
 // GLOBAL VARIABLES
 
 const APIkey = "be9f3e7fb99ef3d5a6cdca04ec93f7de";
-const useDummyOneCallAPIData = false;
+
 export const daysOfTheWeek = [
     "Sunday",
     "Monday",
@@ -17,38 +18,12 @@ export const daysOfTheWeek = [
     "Friday",
     "Saturday"
 ]
-const searchInput = document.querySelector(".search-input");
-
-// EVENT LISTENERS
-
-document.querySelector(".search-button").addEventListener("click", fetch5day3hourAPIData);
-window.addEventListener("keydown", event => {
-    if (event.key === "Enter") {
-        searchInput.blur();
-        fetch5day3hourAPIData();
-    }
-});
-
-searchInput.addEventListener("focus", () => {
-    const cursor = document.querySelector(".cursor");
-    const lookingGlassImg = document.querySelector(".search-button img");
-    
-    cursor.style.display = "none";
-    lookingGlassImg.style.opacity = "1";
-})
-searchInput.addEventListener("blur", () => {
-    const cursor = document.querySelector(".cursor");
-    const lookingGlassImg = document.querySelector(".search-button img");
-    
-    cursor.style.display = "inline";
-    lookingGlassImg.style.opacity = ".5";
-})
-
-window.addEventListener("scroll", () => {
-    const scrolledPx = window.scrollY;
-    const background = document.querySelector(".background");
-    background.style.top = - (scrolledPx * .5) + "px";
-})
+const domElems = {
+    searchInput: document.querySelector(".search-input"),
+    mainElem: document.querySelector("main"),
+    searchButtonImg: document.querySelector(".search-button img"),
+    cursor: document.querySelector(".cursor"),
+}
 
 
 // FUNCTIONS
@@ -58,25 +33,15 @@ issue: background image behind .search-bar and .current-weather needs to reach u
 using `height: 100vh` isn't enough on viewports with a height < .search-bar + .current-weather
 solution: check where .coming-days starts and set that as the background image's height
 */
-window.onload = setBgImgHeight();
-function setBgImgHeight() {
+const setBgImgHeight = () => {
     const comingDaysBgCnt = document.querySelector(".coming-days-bg-cnt");
     document.documentElement.style.setProperty("--comingDaysOffsetTop", comingDaysBgCnt.offsetTop + 40 + "px");
 }
+window.onload = setBgImgHeight();
 
-// run search without triggering an event to not start with an empty page
-fetch5day3hourAPIData();
-function fetch5day3hourAPIData() {
-    removeWeekSection();
-    removeErrorMessage();
-    const searchedLocation = searchInput.value;
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchedLocation}&units=metric&appid=${APIkey}`)
-        .then(response => response.json())
-        .then(weatherData => (parse5day3hourAPIData(weatherData)));
-};
 
-function removeWeekSection() {
+const removeWeekSection = () => {
     const days = document.querySelectorAll(".day");
     
     for (const day of days) {
@@ -87,25 +52,52 @@ function removeWeekSection() {
     }
 }
 
-function removeErrorMessage() {
+
+const removeErrorMessage = () => {
     const errorMessageP = document.querySelector(".error-message");
 
     if (errorMessageP) {
         errorMessageP.remove();
-        const mainElem = document.querySelector("main");
-        mainElem.style.display = "block";
+        domElems.mainElem.style.display = "block";
     }
 }
 
-function parse5day3hourAPIData(weatherData) {
+
+const showErrorMsg = msg => {
+    const errorMsg = document.createElement("p");
+    errorMsg.classList.add("error-message");
+    errorMsg.textContent = msg;
+    
+    document.body.insertBefore(errorMsg, document.body.children[2]);
+    
+    domElems.mainElem.style.display = "none";
+}
+
+
+const parseOneCallAPIData = weatherData => {
+    const currentWeatherData = [];
+    
+    currentWeatherData["summary"] = weatherData.current.weather[0].main;
+    currentWeatherData["description"] = weatherData.current.weather[0].description;
+    currentWeatherData["dateObject"] = new Date(weatherData.current.dt * 1000);
+    currentWeatherData["temperature"] = weatherData.current.temp;
+    currentWeatherData["humidity"] = weatherData.current.humidity;
+    currentWeatherData["precip"] = weatherData.current.pop;
+    currentWeatherData["iconName"] = weatherData.current.weather[0].icon;
+    currentWeatherData["wind-speed"] = weatherData.current.wind_speed; // meter/sec
+    currentWeatherData["wind-direction"] = weatherData.current.wind_deg;
+
+    return (currentWeatherData);
+}
+
+
+const parse5day3hourAPIData = weatherData => {
     if (weatherData.cod !== "200") {
         showErrorMsg(weatherData.message);
         return ;
     }
 
-    fetchOneCallAPIData(weatherData);
-
-    const parsedData = [];
+    const comingDaysData = [];
 
     for (const listItem of weatherData.list) {
         const item = {};
@@ -119,48 +111,95 @@ function parse5day3hourAPIData(weatherData) {
         item["iconName"] = listItem.weather[0].icon;
         item["wind-speed"] = listItem.wind.speed; // meter/sec
         item["wind-direction"] = listItem.wind.deg;
-        parsedData.push(item);
+        comingDaysData.push(item);
     }
 
-    createComingDaysSection(parsedData);
+    return (comingDaysData);
 }
 
-function showErrorMsg(msg) {
-    const errorMsg = document.createElement("p");
-    errorMsg.classList.add("error-message");
-    errorMsg.textContent = msg;
-    
-    document.body.insertBefore(errorMsg, document.body.children[2]);
-    
-    const mainElem = document.querySelector("main");
-    mainElem.style.display = "none";
+
+const fetchOneCallAPIData = async data5d3h => {
+    let dataOneCallAPI;
+    const lat = data5d3h.city.coord.lat;
+    const lon = data5d3h.city.coord.lon;
+
+    await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`)
+        .then(response => response.json())
+        .then(data => dataOneCallAPI = data);
+
+    return (dataOneCallAPI);
 }
 
-function fetchOneCallAPIData(weatherData) {
-    if (useDummyOneCallAPIData) {
-        parseOneCallAPIData(dummyOneCallAPIData);
-    } else {
-        const lat = weatherData.city.coord.lat;
-        const lon = weatherData.city.coord.lon;
+
+const fetch5day3hourAPIData = async () => {
+    removeWeekSection();
+    removeErrorMessage();
+
+    const searchedLocation = domElems.searchInput.value;
+    let data5d3h;
     
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${APIkey}`)
-            .then(response => response.json())
-            .then(weatherData => (parseOneCallAPIData(weatherData)));
+    await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchedLocation}&units=metric&appid=${APIkey}`)
+        .then(response => response.json())
+        .then(data => data5d3h = data)
+    
+        return (data5d3h);
+};
+
+
+async function processSearch() {
+
+    const data5d3hAPI = await fetch5day3hourAPIData();
+
+    if (data5d3hAPI.cod !== "200") {
+        showErrorMsg(data5d3hAPI.message);
+        return ;
     }
-}
 
-function parseOneCallAPIData(weatherData) {
-    const currentWeatherData = [];
+    const dataOneCallAPI = await fetchOneCallAPIData(data5d3hAPI);
+    const currentWeatherData = parseOneCallAPIData(dataOneCallAPI);
     
-    currentWeatherData["summary"] = weatherData.current.weather[0].main;
-    currentWeatherData["description"] = weatherData.current.weather[0].description;
-    currentWeatherData["dateObject"] = new Date(weatherData.current.dt * 1000);
-    currentWeatherData["temperature"] = weatherData.current.temp;
-    currentWeatherData["humidity"] = weatherData.current.humidity;
-    currentWeatherData["precip"] = weatherData.current.pop;
-    currentWeatherData["iconName"] = weatherData.current.weather[0].icon;
-    currentWeatherData["wind-speed"] = weatherData.current.wind_speed; // meter/sec
-    currentWeatherData["wind-direction"] = weatherData.current.wind_deg;
+    // TESTING ---
+    // const useDummyOneCallAPIData = false;
+    // const currentWeatherData = useDummyOneCallAPIData ?
+    // parseOneCallAPIData(dummyOneCallAPIData) :
+    // parseOneCallAPIData(dataOneCallAPI);
+    // ---
     
-    createCurrentWeatherDiv(currentWeatherData, weatherData);
-}
+    createCurrentWeatherDiv(currentWeatherData, dataOneCallAPI);
+    const comingDaysData = parse5day3hourAPIData(data5d3hAPI);
+    createComingDaysSection(comingDaysData);
+};
+
+
+// run search without triggering an event to not start with an empty page
+(function entryPoint() {
+    processSearch();
+}());
+
+
+// EVENT LISTENERS
+
+domElems.searchInput.addEventListener("focus", () => {
+    domElems.cursor.style.display = "none";
+    domElems.searchButtonImg.style.opacity = "1";
+})
+
+domElems.searchInput.addEventListener("blur", () => {
+    domElems.cursor.style.display = "inline";
+    domElems.searchButtonImg.style.opacity = ".5";
+})
+
+document.querySelector(".search-button").addEventListener("click", processSearch);
+
+window.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        domElems.searchInput.blur();
+        processSearch();
+    }
+});
+
+window.addEventListener("scroll", () => {
+    const scrolledPx = window.scrollY;
+    const background = document.querySelector(".background");
+    background.style.top = - (scrolledPx * .5) + "px";
+})
