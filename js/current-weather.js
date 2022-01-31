f1212d8433f220dcb52025d086b1cc88f2a310b5
Magali Fabri <magali_90@live.nl>
2022-01-31 15:38:43 +0100

    Reverse function order
    Preparation for converting from function declarations to arrow functions

--- js/current-weather.js
+++ js/current-weather.js
@@ -2,6 +2,7 @@
 
 import { daysOfTheWeek } from "./script.js";
 
+
 // GLOBAL VARIABLES
 
 const currentWeatherDOMElems = {
@@ -24,81 +25,81 @@ const currentWeatherDOMElems = {
 
 // FUNCTIONS
 
-export function createCurrentWeatherDiv(currentWeatherData, allWeatherData) {
-    fillSummaryP(currentWeatherData);
-    fillMainDataDiv(currentWeatherData);
-    fillTemperatureChart(allWeatherData);
-    fillPrecipitationChart(allWeatherData);
 
-    currentWeatherDOMElems.temperatureChartButton.addEventListener("click", switchCharts);
-    currentWeatherDOMElems.precipitationChartButton.addEventListener("click", switchCharts);
-}
+function switchCharts(event) {
+    currentWeatherDOMElems.temperatureChart.classList.remove("hidden");
+    currentWeatherDOMElems.precipitationChart.classList.remove("hidden");
 
-function fillSummaryP(data) {
-    const day = daysOfTheWeek[data.dateObject.getDay()];
-    const hours = data.dateObject.getHours();
-    let minutes = data.dateObject.getMinutes();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    const description = data.description;
-    
-    currentWeatherDOMElems.summaryP.textContent = `${day} ${hours}:${minutes}, ${description}`;
-}
+    currentWeatherDOMElems.temperatureChartButton.classList.remove("hidden");
+    currentWeatherDOMElems.precipitationChartButton.classList.remove("hidden");
 
-function fillMainDataDiv(data) {
-    currentWeatherDOMElems.bigWeatherIcon.setAttribute("src", `./images/weather-icons/${data.iconName}.png`);
-    currentWeatherDOMElems.bigWeatherIcon.setAttribute("alt", "icon of " + data.description);
-    
-    currentWeatherDOMElems.bigTemperatureNumber.innerHTML = `${Math.round(data.temperature)}<sup>°</sup>`
-
-    currentWeatherDOMElems.precipitationLi.textContent = data.precip | "0";
-    currentWeatherDOMElems.windLi.textContent = Math.round(data["wind-speed"] * 3.6);
-    currentWeatherDOMElems.windDirectionLi.style.transform = `rotate(${-90 + (Math.abs(data["wind-direction"] - 180))}deg)`;
-}
-
-function fillTemperatureChart(weatherData) {
-    setGlobalChartJsOptions();
-    destroyOldChart(currentWeatherDOMElems.temperatureChart.chartInstance);
-    const dataObj = selectDataForTemperatureChart(weatherData)
-    createNewTemperatureChartJsObj(dataObj);
-}
-
-// fillTemperatureChart HELPER FUNCTIONS
-function setGlobalChartJsOptions() {
-    Chart.defaults.font.size = 16;
-    Chart.defaults.color = "white";
-    Chart.defaults.font.family = "Dosis";
-    Chart.defaults.plugins.tooltip.enabled = false;
-    Chart.defaults.plugins.legend.display = false;
-    Chart.defaults.scale.beginAtZero = true;
-    Chart.defaults.scale.grid.display = false;
-    Chart.defaults.scale.grid.drawBorder = false;
-    Chart.defaults.scale.ticks.maxRotation = 0;
-    Chart.defaults.scale.ticks.maxTicksLimit = 6;
-    Chart.defaults.layout.padding = {top: 30, right: 10, left: 10};
-    Chart.defaults.hover.mode = null;
-;}
-
-function destroyOldChart(chart) {
-    if (chart) {
-        chart.destroy();
+    if (event.target.classList[0].includes("temperature")) {
+        currentWeatherDOMElems.precipitationChart.classList.add("hidden");
+        currentWeatherDOMElems.precipitationChartButton.classList.add("hidden");
+    } else {
+        currentWeatherDOMElems.temperatureChart.classList.add("hidden");
+        currentWeatherDOMElems.temperatureChartButton.classList.add("hidden");
     }
 }
 
-function selectDataForTemperatureChart(weatherData) {
+
+// END fillPrecipitationChart HELPER FUNCTIONS
+
+function createNewPrecipitationChartJsObj(dataObj) {
+    currentWeatherDOMElems.precipitationChart.chartInstance = new Chart(currentWeatherDOMElems.precipitationChart, {
+        plugins: [ChartDataLabels],
+        type: 'bar',
+        data: {
+            labels: dataObj.timestamps,
+            datasets: [{
+                barPercentage: 1.0, // no gap between bars
+                categoryPercentage: 1.0, // ^
+                data: dataObj.precipitationData,
+                backgroundColor: "rgba(255, 255, 255, .3)",
+                
+                datalabels: {
+                    anchor: "end",
+                    align: "top",
+                    padding: {
+                        left: 5,
+                        right: 5,
+                        top: 5,
+                    },
+                    display: "auto",
+                    formatter: value => value > 0 ? value + "%" : "",
+                }
+            }]
+        },
+        options: { scales: { y: { ticks: { display: false} } } }
+    });
+}
+
+
+function selectDataForPrecipitationChart(weatherData) {
     const dataObj = {
-        temperatureData: [],
+        precipitationData: [],
         timestamps: [],
     };
     
     for (let i = 0; i < 12; i++) {
-        dataObj.temperatureData.push(Math.round(weatherData.hourly[i].temp));
+        dataObj.precipitationData.push(weatherData.hourly[i].pop * 100);
         dataObj.timestamps.push(new Date(weatherData.hourly[i].dt * 1000).getHours() + "h");
     }
 
     return (dataObj);
 }
+
+// fillPrecipitationChart HELPER FUNCTIONS
+
+
+function fillPrecipitationChart(weatherData) {
+    destroyOldChart(currentWeatherDOMElems.precipitationChart.chartInstance);
+    const dataObj = selectDataForPrecipitationChart(weatherData);
+    createNewPrecipitationChartJsObj(dataObj);
+}
+
+
+// END fillTemperatureChart HELPER FUNCTIONS
 
 function createNewTemperatureChartJsObj(dataObj) {
     currentWeatherDOMElems.temperatureChart.chartInstance = new Chart(currentWeatherDOMElems.temperatureChart, {
@@ -130,71 +131,87 @@ function createNewTemperatureChartJsObj(dataObj) {
         options: { scales: { y: { ticks: { display: false} } } }
     });
 }
-// END fillTemperatureChart HELPER FUNCTIONS
 
-function fillPrecipitationChart(weatherData) {
-    destroyOldChart(currentWeatherDOMElems.precipitationChart.chartInstance);
-    const dataObj = selectDataForPrecipitationChart(weatherData);
-    createNewPrecipitationChartJsObj(dataObj);
-}
 
-// fillTemperatureChart HELPER FUNCTIONS
-function selectDataForPrecipitationChart(weatherData) {
+function selectDataForTemperatureChart(weatherData) {
     const dataObj = {
-        precipitationData: [],
+        temperatureData: [],
         timestamps: [],
     };
     
     for (let i = 0; i < 12; i++) {
-        dataObj.precipitationData.push(weatherData.hourly[i].pop * 100);
+        dataObj.temperatureData.push(Math.round(weatherData.hourly[i].temp));
         dataObj.timestamps.push(new Date(weatherData.hourly[i].dt * 1000).getHours() + "h");
     }
 
     return (dataObj);
 }
 
-function createNewPrecipitationChartJsObj(dataObj) {
-    currentWeatherDOMElems.precipitationChart.chartInstance = new Chart(currentWeatherDOMElems.precipitationChart, {
-        plugins: [ChartDataLabels],
-        type: 'bar',
-        data: {
-            labels: dataObj.timestamps,
-            datasets: [{
-                barPercentage: 1.0, // no gap between bars
-                categoryPercentage: 1.0, // ^
-                data: dataObj.precipitationData,
-                backgroundColor: "rgba(255, 255, 255, .3)",
-                
-                datalabels: {
-                    anchor: "end",
-                    align: "top",
-                    padding: {
-                        left: 5,
-                        right: 5,
-                        top: 5,
-                    },
-                    display: "auto",
-                    formatter: value => value > 0 ? value + "%" : "",
-                }
-            }]
-        },
-        options: { scales: { y: { ticks: { display: false} } } }
-    });
-}
 
-function switchCharts(event) {
-    currentWeatherDOMElems.temperatureChart.classList.remove("hidden");
-    currentWeatherDOMElems.precipitationChart.classList.remove("hidden");
-
-    currentWeatherDOMElems.temperatureChartButton.classList.remove("hidden");
-    currentWeatherDOMElems.precipitationChartButton.classList.remove("hidden");
-
-    if (event.target.classList[0].includes("temperature")) {
-        currentWeatherDOMElems.precipitationChart.classList.add("hidden");
-        currentWeatherDOMElems.precipitationChartButton.classList.add("hidden");
-    } else {
-        currentWeatherDOMElems.temperatureChart.classList.add("hidden");
-        currentWeatherDOMElems.temperatureChartButton.classList.add("hidden");
+function destroyOldChart(chart) {
+    if (chart) {
+        chart.destroy();
     }
 }
-// END fillTemperatureChart HELPER FUNCTIONS
+
+
+function setGlobalChartJsOptions() {
+    Chart.defaults.font.size = 16;
+    Chart.defaults.color = "white";
+    Chart.defaults.font.family = "Dosis";
+    Chart.defaults.plugins.tooltip.enabled = false;
+    Chart.defaults.plugins.legend.display = false;
+    Chart.defaults.scale.beginAtZero = true;
+    Chart.defaults.scale.grid.display = false;
+    Chart.defaults.scale.grid.drawBorder = false;
+    Chart.defaults.scale.ticks.maxRotation = 0;
+    Chart.defaults.scale.ticks.maxTicksLimit = 6;
+    Chart.defaults.layout.padding = {top: 30, right: 10, left: 10};
+    Chart.defaults.hover.mode = null;
+;}
+
+// fillTemperatureChart HELPER FUNCTIONS
+
+
+function fillTemperatureChart(weatherData) {
+    setGlobalChartJsOptions();
+    destroyOldChart(currentWeatherDOMElems.temperatureChart.chartInstance);
+    const dataObj = selectDataForTemperatureChart(weatherData)
+    createNewTemperatureChartJsObj(dataObj);
+}
+
+
+function fillMainDataDiv(data) {
+    currentWeatherDOMElems.bigWeatherIcon.setAttribute("src", `./images/weather-icons/${data.iconName}.png`);
+    currentWeatherDOMElems.bigWeatherIcon.setAttribute("alt", "icon of " + data.description);
+    
+    currentWeatherDOMElems.bigTemperatureNumber.innerHTML = `${Math.round(data.temperature)}<sup>°</sup>`
+
+    currentWeatherDOMElems.precipitationLi.textContent = data.precip | "0";
+    currentWeatherDOMElems.windLi.textContent = Math.round(data["wind-speed"] * 3.6);
+    currentWeatherDOMElems.windDirectionLi.style.transform = `rotate(${-90 + (Math.abs(data["wind-direction"] - 180))}deg)`;
+}
+
+
+function fillSummaryP(data) {
+    const day = daysOfTheWeek[data.dateObject.getDay()];
+    const hours = data.dateObject.getHours();
+    let minutes = data.dateObject.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    const description = data.description;
+    
+    currentWeatherDOMElems.summaryP.textContent = `${day} ${hours}:${minutes}, ${description}`;
+}
+
+
+export function createCurrentWeatherDiv(currentWeatherData, allWeatherData) {
+    fillSummaryP(currentWeatherData);
+    fillMainDataDiv(currentWeatherData);
+    fillTemperatureChart(allWeatherData);
+    fillPrecipitationChart(allWeatherData);
+
+    currentWeatherDOMElems.temperatureChartButton.addEventListener("click", switchCharts);
+    currentWeatherDOMElems.precipitationChartButton.addEventListener("click", switchCharts);
+}
